a3bc91e24643ba1035a12f70467f0346de69dfc7
David Boclé <bcldvd@gmail.com>
2014-05-22 22:13:16 +0200

    feat (implement RPC and Topic)
    Functionnal chat
    Add of bootbox prompt to ask for nickname
    Nickname sent to the websocket server
    Room general is joined and client listens to event on it
    Server answers and broadcast messages

--- src/Acme/ChatBundle/Resources/public/js/main.js
+++ src/Acme/ChatBundle/Resources/public/js/main.js
@@ -1,5 +1,98 @@
+var _room = 'chat/general';
+var nickname = 'Guest';
+
 $(document).ready(function() {
 
-  var network = Clank.connect(_CLANK_URI);
+  // Ask for our name and calls client 'Anonymous' if he entered nothing
+  bootbox.prompt("What is your name?", function(result) {
+    if (result === null) {
+      nickname = 'Anonymous';
+    } else {
+      nickname = result;
+    }
+
+    // Set the newly appointed nickname
+    $('#nickname').html(nickname);
+
+    // Connect to Clank
+    var network = Clank.connect(_CLANK_URI);
+
+    network.on("socket/connect", function(session){
+        setNickname(session);
+        subscribeToRoom(session, _room);
+        $('#panelChat .panel-heading').html('You are connected to the General channel');
+        console.log('Connected to : '+_room);
+        bindUi(session);
+    });
+
+    network.on("socket/disconnect", function(session){
+        $('#panelChat .panel-heading').html('You are not connected to any channel');
+        console.log('disconnected to : '+_room);
+        unbindUi();
+    });
+
+  });
 
 });
+
+/**
+ * Below is the Clank relevant functions, the rest of this file is UI Bindings etc.
+ */
+
+function setNickname(session){
+  session.call("chat/change_nickname", {nickname: nickname});
+}
+
+function subscribeToRoom(session, room) {
+    session.subscribe(_room, function(uri, payload){
+        appendChat(payload.from, payload.msg, payload.system);
+    });
+}
+
+
+function publishChat(session) {
+    if (!_room)
+        return;
+
+    var msg = $("#chat-input").val();
+    $("#chat-input").val("");
+
+    session.publish(_room, msg);
+}
+
+
+function appendChat(from, msg, system) {
+    if(system === true){
+      $(".panel-body").append('<div class="alert alert-info">' + msg + '</div>');
+    }else{
+      $(".panel-body").append("<div><span>"+from+"</span>: " + msg + "</div>");
+    }
+    $(".panel-body").scrollTop($('.panel-body').get(0).scrollHeight);
+}
+
+
+function bindUi(session) {
+    $("#send-chat").bind("click", function(e){
+        e.preventDefault();
+        publishChat(session)
+    });
+
+    $("#chat-input").bind("keypress", function(e){
+        var code = (e.keyCode ? e.keyCode : e.which);
+        if(code == 13) { //Enter keycode
+            publishChat(session);
+        }
+    });
+}
+
+// attempt to clean up on disconnect.
+function unbindUi() {
+    $("#join-chat").unbind();
+    $("#chatroom").unbind();
+
+    $("#send-chat").unbind();
+    $("#chat-input").unbind();
+
+    $("#nickname > input").unbind();
+    $("#nickname > a").unbind();
+}
---
+++ src/Acme/ChatBundle/Topic/ChatTopic.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Acme\ChatBundle\Topic;
+
+use Ratchet\ConnectionInterface as Conn;
+
+use JDare\ClankBundle\Server\App\Handler\TopicHandlerInterface;
+
+class ChatTopic implements TopicHandlerInterface
+{
+    /**
+     * Announce to this topic that someone else has joined the chat room
+     *
+     * Also, set their nickname to Guest if it doesnt exist.
+     *
+     * @param \Ratchet\ConnectionInterface $conn
+     * @param $topic
+     */
+    public function onSubscribe(Conn $conn, $topic)
+    {
+        if (!isset($conn->ChatNickname))
+        {
+            $conn->ChatNickname = "Guest";
+        }
+
+        $msg = $conn->ChatNickname . " joined the chat room.";
+
+        $topic->broadcast(array("msg" => $msg, "from" => "System", "system" => true));
+    }
+
+    /**
+     * Announce person left chat room
+     *
+     * @param \Ratchet\ConnectionInterface $conn
+     * @param $topic
+     */
+    public function onUnSubscribe(Conn $conn, $topic)
+    {
+        $msg = $conn->ChatNickname . " left the chat room.";
+
+        $topic->broadcast(array("msg" => $msg, "from" => "System", "system" => true));
+    }
+
+    /**
+     * Do some nice things like strip html/xss etc. then pass along the message
+     *
+     * @param \Ratchet\ConnectionInterface $conn
+     * @param $topic
+     * @param $event
+     * @param array $exclude
+     * @param array $eligible
+     */
+    public function onPublish(Conn $conn, $topic, $event, array $exclude, array $eligible)
+    {
+        $event = htmlentities($event); // removing html/js
+
+        $topic->broadcast(array("msg" => $event, "from" => $conn->ChatNickname));
+    }
+}
